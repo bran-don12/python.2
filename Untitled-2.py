5600e75fdc2df72efb8aff6b35ecbf39efdb9cc3
bran-don12 <107563806+bran-don12@users.noreply.github.com>
2022-09-03 23:22:36 -0500

    Add files via upload

---
+++ Untitled-2.py
@@ -0,0 +1,35 @@
+vuelta=nos
+while nos<10:
+print ( 'buen dia, digite los siguientes datos para realizar la nomina' )
+
+imprimir ( "Documento:" )
+
+D = int ( entrada ())
+
+print ( "Nombres:" )
+
+N = cadena ( entrada ())
+
+print ( "Apellidos:" )
+
+A = cadena ( entrada ())
+
+print ( "¿cual es su salario?" )
+
+sl = int ( entrada ())
+
+print ( "¿cuantos dias trabajaron?" )
+
+Dt = int ( entrada ())
+
+imprimir ( "NOMINA" )
+
+tr = 117100 / 30
+sb = tr * Dt
+vld = sl / 30
+st = vld * Dt
+dps = punto * 8 / 100
+st_d = st + sb - dps
+
+print ( "El señor/a" , N , A , "con numero de documento " , D , ",que recibe por contrato el monto de " , sl , "y que trabajo" , Dt , "dias el mes de julio, sumado al subsidio transporte por dias trabajados, y restando las prestaciones sociales, se le consigna un total de: " , st_d )
+file 
